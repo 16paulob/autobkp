b66dba7783a9a4e2ea09af7fb232c8db92361958
16paulob <38596525+16paulob@users.noreply.github.com>
2019-08-06 21:34:59 -0300

    I inserted a new permission change statement.

--- autotransf.sh
+++ autotransf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-  rsync -abz --remove-source-files --include=*.bmp --exclude=* '/src_folder/' '/dst_folder/'
+  rsync -abz --remove-source-files --include=*.bmp --exclude=* --chmod=755 '/src_folder/' '/dst_folder/'
   sleep 5;
 done
